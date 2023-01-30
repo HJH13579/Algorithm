09779daf76a014e06fb703b26fb9c23572f1ab12
HJH13579 <heojoohyeok@naver.com>
2023-01-30 17:56:03 +0900

    add SWEA_1206

---
+++ SWEA_1206.py
@@ -0,0 +1,24 @@
+for task in range(1, 11):
+
+    N = int(input())
+    M = list(map(int, input().split()))
+
+    building = {} # 건물의 위치(x축)를 key로, 건물의 높이(y축)를 value로 하는 딕셔너리
+
+    for x in range(N):
+        building[x] = M[x] # x번째 빌딩은 M[x]층
+
+    def praise_the_sun():
+        sum_light_floor = 0
+
+        for n in range(2, N-2): # n-2, n-1, n, n+1, n+2 로 좌우 2칸까지 빌딩과 높이 비교
+            if building[n] <= building[n-2] or building[n] <= building[n-1] or building[n] <= building[n+1] or building[n] <= building[n+2]: # n번째 빌딩 좌우 2칸까지 어느 하나라도 더 높으면 조망층 0개
+                pass
+            else:
+                lst_light_floor = sorted([building[n]-building[n-2], building[n]-building[n-1], building[n]-building[n+1], building[n]-building[n+2]]) # 가장 작은 값이 조망층 개수, sorting
+                light_floor = lst_light_floor[0] # 오름차순 list, 0번째가 최소
+                sum_light_floor = sum_light_floor + light_floor # 조망층 합
+
+        return sum_light_floor
+
+    print(f'#{task} {praise_the_sun()}')
