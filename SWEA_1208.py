7403e0ca559a993915c66607ae7ace0f6f7db690
HJH13579 <heojoohyeok@naver.com>
2023-01-31 17:29:06 +0900

    add SWEA_1208

---
+++ SWEA_1208.py
@@ -0,0 +1,34 @@
+for task in range(1, 110):
+    
+    dump_num = int(input())
+    height = list(map(int, input().split()))
+
+    sorted_height = sorted(height)
+    
+
+    while (1):
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    print(f'#{task} {}')
