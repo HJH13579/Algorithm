bcee3e882588595ba61400cf062d57519d98f710
HJH13579 <heojoohyeok@naver.com>
2023-02-14 17:55:11 +0900

    for check

--- BOJ_2477/BOJ_2477.py
+++ BOJ_2477/BOJ_2477.py
@@ -9,10 +9,26 @@ N = int(input())
 # 그 2번씩 반복되는 움직임 중 사이에 있는 움직임이 들어간 꼭짓점
 # 반대로 1번씩 움직이는 길이가 최대
 
-vertex_lst = []
+lst_vertex = []
+lst_dir = [0] * 5 # direction number을 count하는 리스트
 
 for _ in range(6):
     vertex = list(map(int, input().split()))
-    vertex_lst.append(vertex)
+    lst_vertex.append(vertex)
+
+for i in range(5):
+    for j in range(6):
+        if lst_vertex[j][0] == i:
+            lst_dir[i] += 1
+
+max_square_area = 1
+
+for x in range(5):
+    if lst_dir[x] == 1:
+        for y in 
+
+
+
+print(lst_vertex)
 
 
