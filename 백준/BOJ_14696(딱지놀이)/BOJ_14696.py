2fc32959c80e3db4bebd9bfa36657776ab127bd2
HJH13579 <heojoohyeok@naver.com>
2023-03-22 17:54:28 +0900

    add BOJ_14696

--- 백준/BOJ_14696(딱지놀이)/BOJ_14696.py
+++ 백준/BOJ_14696(딱지놀이)/BOJ_14696.py
@@ -1,9 +1,42 @@
 import sys
 sys.stdin = open("input.txt", "r")
 
-def winner_winner_chicken_dinner(lst_A, lst_B):
-    for x
+def game(lst_A, lst_B, n):
+    if n != 1:
+        cnt_n_a = 0
+        cnt_n_b = 0
+        for x in lst_A[1:]:
+            if x == n:
+                cnt_n_a += 1
+        for y in lst_B[1:]:
+            if y == n:
+                cnt_n_b += 1
 
+        if cnt_n_a == cnt_n_b:
+            return game(lst_A, lst_B, n-1)
+        else:
+            if cnt_n_a < cnt_n_b:
+                return 'B'
+            elif cnt_n_a > cnt_n_b:
+                return 'A'
+
+    else:
+        cnt_1_a = 0
+        cnt_1_b = 0
+        for x in lst_A[1:]:
+            if x == 1:
+                cnt_1_a += 1
+        for y in lst_B[1:]:
+            if y == 1:
+                cnt_1_b += 1
+
+        if cnt_1_a == cnt_1_b:
+            return 'D'
+        else:
+            if cnt_1_a < cnt_1_b:
+                return 'B'
+            elif cnt_1_a > cnt_1_b:
+                return 'A'
 
 for tck in range(2):
     N = int(input())
@@ -12,3 +45,4 @@ for tck in range(2):
         lst_A = list(map(int, input().split()))
         lst_B = list(map(int, input().split()))
 
+        print(game(lst_A, lst_B, 4))
